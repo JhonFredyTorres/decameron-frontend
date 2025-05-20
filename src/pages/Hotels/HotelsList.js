4e4875aefd1205369c795cec3263e782ca690477
jhon fredy torres peña <jhonfredytorresp@gmail.com>
2025-05-20 01:31:56 -0500

    errors  in production build

--- src/pages/Hotels/HotelsList.js
+++ src/pages/Hotels/HotelsList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AppContext } from '../../context/AppContext';
 import { hotelService } from '../../services/api';
